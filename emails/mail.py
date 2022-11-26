ee7af6a2580b729b88754f6605bb91262be8ed97
Jake Quinter <hello@jakequinter.io>
2022-11-26 10:02:44 -0500

    Shorten prompts

--- emails/mail.py
+++ emails/mail.py
@@ -12,9 +12,9 @@ HOST = os.getenv("HOST")
 
 
 def get_email_info():
-    recipient_email = input("Enter the email address you wish to send this email: ")
-    subject = input("Enter a subject: ")
-    body = input("Enter the message body: ")
+    recipient_email = input("To: ")
+    subject = input("Subject: ")
+    body = input("Body: ")
     message_to_send = f"Subject: {subject}\n{body}"
 
     return recipient_email, message_to_send
